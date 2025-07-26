c6d3f74d37e89805fac1faa65c17d1407de36fbf
Sravan Kumar <sravanpatel0017@gmail.com>
2025-07-26 10:00:31 +0530

    added env data

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -35,7 +35,7 @@ export default function Register() {
 
     const onSubmit = async ({ username, email, password }) => {
         try {
-            const res = await fetch("http://localhost:5000/api/auth/register", {
+            const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ username, email, password }),
